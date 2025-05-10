b4f13364224da6dea49aed01c64c5b01c4c103cc
arigaran-m <arigaranpdy@gmail.com>
2025-05-10 13:52:40 +0530

    modify: Modified config.py

--- config.py
+++ config.py
@@ -3,9 +3,9 @@ from datetime import timedelta
 class Config:
 
     PERMANENT_SESSION_LIFETIME = timedelta(days = 1)
-    SECRET_KEY = "123456789"
+    SECRET_KEY = "Enter the Secret Key"
     MYSQL_HOST = "localhost"
     MYSQL_USER = "root"
-    MYSQL_PASSWORD = "Ari@2003"
+    MYSQL_PASSWORD = "Enter the MySQL Password"
     MYSQL_DB = "company"
     MYSQL_CURSORCLASS = "DictCursor"
